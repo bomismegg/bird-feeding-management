ab1e9126b63ec1516a7eb34ee3c0ebebc8764890
Pham Nhat Linh <phamnhatlinh20303@gmail.com>
2023-11-06 23:17:57 +0700

    add form

---
+++ FeedingManagement/CageListForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FeedingManagement
+{
+    partial class CageListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.Control;
+            groupBox1.CausesValidation = false;
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(25, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.RightToLeft = RightToLeft.Yes;
+            groupBox1.Size = new Size(421, 412);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(131, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 31);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+
+            // 
+            // CageListForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "CageListForm";
+            Text = "CageListForm";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+    }
+}
---
+++ FeedingManagement/FeedingPlanListForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace FeedingManagement
+{
+    partial class FeedingPlanListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)chartControl1).BeginInit();
+            SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            chartControl1.Location = new Point(153, 266);
+            chartControl1.Name = "chartControl1";
+            chartControl1.Size = new Size(375, 250);
+            chartControl1.TabIndex = 0;
+            // 
+            // FeedingPlanListForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(926, 671);
+            Controls.Add(chartControl1);
+            Name = "FeedingPlanListForm";
+            Text = "FeedingPlanListForm";
+            ((System.ComponentModel.ISupportInitialize)chartControl1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+    }
+}
